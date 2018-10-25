b8ec849f4285e2d83dc4b17e8728ab12b8e1365d
kl4532 <kornellukasiak@gmail.com>
2018-10-25 21:49:58 +0200

    fire_safety_game algoritm

--- own-projects/fire_safety_game/index.js
+++ own-projects/fire_safety_game/index.js
@@ -1,32 +1,23 @@
 var container = document.querySelector("#container");
 var frame = document.querySelector("#frame");
-var dead = false;
+var saved=0, death=0, empty =0;
 //the 2d array that defines the board
 var board = [];
 var arr_qa = [];
+var current;
 function rand(min, max){
   return Math.floor(Math.random()*(1+max-min))+min;
 }
 for(var i=0; i<25;i++){
-  var a = rand(0, 60);
-  var b = rand(0, 60);
-  var answers = [a+b-1, b+a+1, a];
-  arr_qa.push(["Calculate: " + a + " + " + b, a+b, answers])
+  var isPersonInside = false
+  var a = rand(0, 60), b = rand(0, 60), factor = rand(2, 3);
+  if(factor==2){
+    isPersonInside = true;}else isPersonInside = false;
+
+  var answers = [a+b-1, b+a+1, a+b];
+  arr_qa.push(["Calculate: " + a + " + " + b, a+b, answers, isPersonInside]);
+  board.push([i,arr_qa[i][0],arr_qa[i][1],arr_qa[i][2],arr_qa[i][3]] ); //pushing index of question and the question and the answer
 }
-for(var i=0; i<25; i++)
-{
-  board.push([i,arr_qa[i][0],arr_qa[i][1],arr_qa[i][2]]);    //pushing index of question and the question and the answer
-}
-
-//the size of each cell
-var SIZE = 100;
-//the space between each cell
-var SPACE = 10;
-
-//display the array
-var ROWS = board.length;
-// var COLUMNS = board[0].length;
-
 // function setting dimension of container and cell
 function buildingConstructor(players){
   var container = document.getElementById('container');
@@ -49,7 +40,6 @@ function buildingConstructor(players){
   }
 
 }
-
     for(var i = 0; i < board.length; i++){
         //create a div HTML element called cell
         var cell = document.createElement("div");
@@ -58,13 +48,8 @@ function buildingConstructor(players){
         cell.setAttribute("id", i);
         //add the div HTML element to the stage
         container.appendChild(cell);
-
-        //position the cell
-        // cell.style.top = row * (SIZE + SPACE) + "px";
-        // cell.style.left = column * (SIZE + SPACE) + "px";
-
         //handle click
-        cell.addEventListener("click", clickHandler, false);
+        cell.addEventListener("click", clickHandler);
     }
     var entrence = document.createElement("img");
     entrence.setAttribute("class", "entrence");
@@ -74,17 +59,55 @@ function buildingConstructor(players){
 // create qaa div
     var qAndA = document.createElement("div");
     qAndA.setAttribute("class", "qAndA");
-    qAndA.innerHTML = "QAA";
-    qAndA.style.position = "absolute";
-    qAndA.style.top = "10px";
-    qAndA.style.left = "400px";
+    qAndA.innerHTML = "Live saved: "+ saved;
+
     frame.appendChild(qAndA);
-
-
 function clickHandler(){
-      var str =  board[this.id][0] + board[this.id][1] + "=" + board[this.id][2] ;
-      qAndA.innerHTML = "<div>"+str+ "</div>"+board[this.id][3];
-      this.style.backgroundColor = "green";
-
+      current = board[this.id]
+      var str = board[this.id][1];
+      // qAndA.innerHTML = "<div>" + str + "</div>" +"<br>"+ "a) " + wrapInAdiv(board[this.id][3][0])
+      // + "<br>" + "b) " + wrapInAdiv(board[this.id][3][1]) + "<br>" +  "c) " + wrapInAdiv(board[this.id][3][2]);
+      var answer = prompt(str);
+if (answer == board[this.id][2]) {
+  alert("true");
+  this.style.backgroundColor = "blue";
+  this.removeEventListener("click", clickHandler);
+      if(board[this.id][4]==true){
+        ++saved;
+        qAndA.innerHTML = "Live saved: "+ saved + "<br>" + "Death: "+ death + "<br>" + "Empty" + empty;
+        this.style.backgroundColor = "green";
+      }else{
+        ++empty;
+        qAndA.innerHTML = "Live saved: "+ saved + "<br>" + "Death: "+ death + "<br>" + "Empty" + empty;s
+      }
+  //   // if false => mark black, add score, deactivate, do not change player
+}else{
+  alert("false");
+  this.style.backgroundColor = "blue";
+  this.removeEventListener("click", clickHandler);
+  if(board[this.id][4]==true){
+    ++death;
+    qAndA.innerHTML = "Live saved: "+ saved + "<br>" + "Death: "+ death + "<br>" + "Empty" + empty;
+    this.style.backgroundColor = "black";
+  }else{
+    ++empty;
+    qAndA.innerHTML = "Live saved: "+ saved + "<br>" + "Death: "+ death + "<br>" + "Empty" + empty;
+  }
+  //   // if false => mark black, deactivate, change player
+}
     console.log("worked");
 }
+// function wrapInAdiv(value){
+//   return "<span class='answers' onclick='checkAnswer("+value+")'>" + value + "</span>" + "<br>";
+// }
+// function checkAnswer(value){
+// if(current[2]==value){
+//   alert("true");
+//   board.style.backgroundColor = "green";
+//
+//   // if true => mark green, deactivate, dont change the player
+// }else{
+//   alert("false");
+//   // if false => mark black, deactivate, change player
+// }
+// }
