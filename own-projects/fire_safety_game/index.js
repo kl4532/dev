2134ae17db34a224d7e120ddabca67044f598953
kl4532 <kornellukasiak@gmail.com>
2018-10-29 19:08:03 +0100

    firewall_edu_game_v2[PL]

--- own-projects/fire_safety_game/index.js
+++ own-projects/fire_safety_game/index.js
@@ -2,7 +2,6 @@
 var frame = document.querySelector("#frame");   // frame for all divs
 var qAndA = document.createElement("div");     // create qaa div need to be global
 qAndA.setAttribute("class", "qAndA");
-<<<<<<< HEAD
 var saved=0, death=0, empty =0, saved1=0, death1=0, empty1=0, saved2=0, death2=0, empty2=0;
 var windowsNumber=25;
 var board = [], arr_qa = [];
@@ -10,20 +9,10 @@ var current;
 var onePlayer =false;
 var firstPlayer =true;
 var gameInfo ="Uratuj ludzi z płonącego budynku. Przeszukaj wszystkie pomieszczenia klikając na okna i odpowiadaj na pytania. Powodzenia!";
-=======
-var saved=0, death=0, empty =0;
-var windowsNumber=25;
-var board = [];
-var arr_qa = [];
-var current;
-var onePlayer =0;
-var gameInfo ="Uratuj ludzi z płonącego budynku. Przeszukaj wszystkie pomieszczenia klikając na okna i odpowiadając na pytania. Powodzenia!";
->>>>>>> c2288af29f51834a06e2ad7d6b58a6df7c982c53
 
 function rand(min, max){  // random num generator
   return Math.floor(Math.random()*(1+max-min))+min;
 }
-<<<<<<< HEAD
 function output(stat, qAndA, gameInfo, saved, death, empty, players_stat){
   if(!onePlayer){
     stat.innerHTML = gameInfo;
@@ -42,12 +31,6 @@ function output(stat, qAndA, gameInfo, saved, death, empty, players_stat){
     stat.style.left ="300px";  // if one player -> set new narrower positon for div+
     stat.appendChild(players_stat);
   }
-=======
-function output(stat, qAndA, gameInfo, saved, death, empty){
-  stat.innerHTML = gameInfo + `<ul><li class='dotSaved'>Ocaleni: ${saved}</li><li class='dotDeath'>Ofiary: ${death}</li><li class='dotEmpty'>Pusty: ${empty}</li></ul>`;
-  frame.appendChild(stat);
-  onePlayer ? stat.style.left ="300px": stat.style.left ="370px"; // if one player -> set new position
->>>>>>> c2288af29f51834a06e2ad7d6b58a6df7c982c53
 }
 // setting dimensions of container and cell
 function buildingConstructor(players){                  // setting building dimensions for diff amount of players
@@ -104,7 +87,6 @@ function init(players){        // initialization of the game
   var stat = document.createElement("div");        // create status div
   stat.setAttribute("class", "stat");
   stat.setAttribute("id", "stat");
-<<<<<<< HEAD
   //output(stat, qAndA, gameInfo, saved, death, empty);
 
   var players_stat = document.createElement("div");        // create status div
@@ -113,9 +95,6 @@ function init(players){        // initialization of the game
   output(stat, qAndA, gameInfo, saved, death, empty, players_stat);
                                                    // highlite active player
   document.getElementById('player1').style.backgroundColor = "yellow";
-=======
-  output(stat, qAndA, gameInfo, saved, death, empty);
->>>>>>> c2288af29f51834a06e2ad7d6b58a6df7c982c53
 }
 function clickHandler(){
   current = board[this.id];
@@ -124,7 +103,6 @@ function clickHandler(){
     "<br>" + "b) " + wrapInAdiv(current[3][1], this.id) +
     "<br>" +  "c) " + wrapInAdiv(current[3][2], this.id);
   stat.appendChild(qAndA);
-<<<<<<< HEAD
   if(firstPlayer){                                                    // highlite active player
     document.getElementById('player1').style.backgroundColor = "yellow";
     document.getElementById('player2').style.backgroundColor = "transparent";
@@ -133,8 +111,6 @@ function clickHandler(){
     document.getElementById('player2').style.backgroundColor = "yellow";
     document.getElementById('player1').style.backgroundColor = "transparent";
   firstPlayer = true;}
-=======
->>>>>>> c2288af29f51834a06e2ad7d6b58a6df7c982c53
 }
 
 function wrapInAdiv(value, id){
@@ -142,7 +118,6 @@ function wrapInAdiv(value, id){
 }
 function checkAnswer(value, id){
   curr_window = document.getElementById(id);
-<<<<<<< HEAD
   if(current[2]==value){    // check if answer true
     //alert("true");
     curr_window.removeEventListener("click", clickHandler);
@@ -150,53 +125,29 @@ function checkAnswer(value, id){
           ++saved;
           firstPlayer ?  ++saved2 : ++saved1;
           output(stat, qAndA, gameInfo, saved, death, empty, players_stat);
-=======
-  if(current[2]==value){
-    alert("true");
-    curr_window.removeEventListener("click", clickHandler);
-      if(current[4]==true){
-          ++saved;
-          output(stat, qAndA, gameInfo, saved, death, empty);
->>>>>>> c2288af29f51834a06e2ad7d6b58a6df7c982c53
           curr_window.style.backgroundColor = "green";
           curr_window.style.backgroundImage = "none";
       }else{
           ++empty;
-<<<<<<< HEAD
           firstPlayer ?  ++empty2 : ++empty1;
           output(stat, qAndA, gameInfo, saved, death, empty, players_stat);
-=======
-          output(stat, qAndA, gameInfo, saved, death, empty);
->>>>>>> c2288af29f51834a06e2ad7d6b58a6df7c982c53
           curr_window.style.backgroundColor = "black";
           curr_window.style.backgroundImage = "none";
       }
   // if true => mark green, deactivate, dont change the player
 }else{
-<<<<<<< HEAD
   //alert("false");
     curr_window.removeEventListener("click", clickHandler);
     if(current[4]==true){
       ++death;
       firstPlayer ?  ++death2 : ++death1;
       output(stat, qAndA, gameInfo, saved, death, empty, players_stat);
-=======
-  alert("false");
-    curr_window.removeEventListener("click", clickHandler);
-    if(current[4]==true){
-      ++death;
-      output(stat, qAndA, gameInfo, saved, death, empty);
->>>>>>> c2288af29f51834a06e2ad7d6b58a6df7c982c53
       curr_window.style.backgroundColor = "red";
       curr_window.style.backgroundImage = "none";
     }else{
       ++empty;
-<<<<<<< HEAD
       firstPlayer ?  ++empty2 : ++empty1;
       output(stat, qAndA, gameInfo, saved, death, empty, players_stat);
-=======
-      output(stat, qAndA, gameInfo, saved, death, empty);
->>>>>>> c2288af29f51834a06e2ad7d6b58a6df7c982c53
       curr_window.style.backgroundColor = "black";
       curr_window.style.backgroundImage = "none";
     }
@@ -204,7 +155,6 @@ function checkAnswer(value, id){
 }
 if(death+saved+empty==windowsNumber){
   let total = ((saved/(saved+death))*100);
-<<<<<<< HEAD
   if(!onePlayer){
     if(death == 0){
       qAndA.innerHTML = "GRAULACJE! Wspólnie ocaliliście wszystkich mieszkańców znajdujących się w budynku :) <br>KONIEC(odśwież stronę by zagrać ponownie)";
@@ -230,17 +180,5 @@ if(death+saved+empty==windowsNumber){
     stat.appendChild(qAndA);
   }
 }
-=======
-  if(death == 0){
-    qAndA.innerHTML = "GRAULACJE! Ocaliłeś wszystkich mieszkańców znajdujących się w budynku :) <br>KONIEC(odśwież stronę by zagrać ponownie)";
-    stat.appendChild(qAndA);
-  }else if(saved == 0){
-    qAndA.innerHTML = ("Wszyscy potrzebujący pomocy zginęli... <br>KONIEC￼(odśwież stronę by zagrać ponownie)￼");
-    stat.appendChild(qAndA);
-  }else {
-    qAndA.innerHTML = ("Ocaliłeś " + total.toFixed(0) + "% mieszkańców znajdujących się w budynku <br>KONIEC￼(odśwież stronę by zagrać ponownie)");
-    stat.appendChild(qAndA);
-  }
->>>>>>> c2288af29f51834a06e2ad7d6b58a6df7c982c53
 }
 }
