12f12c72f68d0aa7b81ba369e9bf9ec90dd5341d
kl4532 <kornellukasiak@gmail.com>
2018-10-29 19:00:24 +0100

    firewall_edu_game_v2[PL]

--- own-projects/fire_safety_game/index.js
+++ own-projects/fire_safety_game/index.js
@@ -1,113 +1,184 @@
-var container = document.querySelector("#container");
-var frame = document.querySelector("#frame");
-var saved=0, death=0, empty =0;
-//the 2d array that defines the board
-var board = [];
-var arr_qa = [];
+
+var frame = document.querySelector("#frame");   // frame for all divs
+var qAndA = document.createElement("div");     // create qaa div need to be global
+qAndA.setAttribute("class", "qAndA");
+var saved=0, death=0, empty =0, saved1=0, death1=0, empty1=0, saved2=0, death2=0, empty2=0;
+var windowsNumber=25;
+var board = [], arr_qa = [];
 var current;
-function rand(min, max){
+var onePlayer =false;
+var firstPlayer =true;
+var gameInfo ="Uratuj ludzi z płonącego budynku. Przeszukaj wszystkie pomieszczenia klikając na okna i odpowiadaj na pytania. Powodzenia!";
+
+function rand(min, max){  // random num generator
   return Math.floor(Math.random()*(1+max-min))+min;
 }
-for(var i=0; i<25;i++){
-  var isPersonInside = false
-  var a = rand(0, 60), b = rand(0, 60), factor = rand(2, 3);
-  if(factor==2){
-    isPersonInside = true;}else isPersonInside = false;
-
-  var answers = [a+b-1, b+a+1, a+b];
-  arr_qa.push(["Calculate: " + a + " + " + b, a+b, answers, isPersonInside]);
-  board.push([i,arr_qa[i][0],arr_qa[i][1],arr_qa[i][2],arr_qa[i][3]] ); //pushing index of question and the question and the answer
+function output(stat, qAndA, gameInfo, saved, death, empty, players_stat){
+  if(!onePlayer){
+    stat.innerHTML = gameInfo;
+    frame.appendChild(stat);
+    players_stat.innerHTML = "<div id='player1'>Gracz 1</div>"+
+    `<ul><li class='dotSaved'>Ocaleni: ${saved1}</li><li class='dotDeath'>Ofiary: ${death1}</li><li class='dotEmpty'>Pusty: ${empty1}</li></ul>`+
+    "<div id = 'player2'>Gracz 2</div>"+
+    `<ul><li class='dotSaved'>Ocaleni: ${saved2}</li><li class='dotDeath'>Ofiary: ${death2}</li><li class='dotEmpty'>Pusty: ${empty2}</li></ul>`;
+    stat.appendChild(players_stat);
+    if(firstPlayer){
+      document.getElementById('player1').style.backgroundColor = "yellow";
+    }else document.getElementById('player2').style.backgroundColor = "yellow";
+  }else{
+    stat.innerHTML = gameInfo + `<ul><li class='dotSaved'>Ocaleni: ${saved}</li><li class='dotDeath'>Ofiary: ${death}</li><li class='dotEmpty'>Pusty: ${empty}</li></ul>`;
+    frame.appendChild(stat);
+    stat.style.left ="300px";  // if one player -> set new narrower positon for div+
+    stat.appendChild(players_stat);
+  }
 }
-// function setting dimension of container and cell
-function buildingConstructor(players){
+// setting dimensions of container and cell
+function buildingConstructor(players){                  // setting building dimensions for diff amount of players
   var container = document.getElementById('container');
-  switch (players) {
+  switch (players){
     case 1:
-      container.style.width = "280px";    // 4x4  //280x500px;
+      container.style.width = "280px";    // 4x4 //280x500px;
       container.style.height = "500px";
+      windowsNumber=16;
+      onePlayer=true;
       break;
     case 2:
-      container.style.width = "350px";    // 4x8
+      container.style.width = "350px";    // 5x5
       container.style.height = "600px";
-      break;
-    case 3:
-      container.style.width = "900px";  // 6x8
-      break;
-    case 4:
-      container.style.width = "1200px"; // 8x10
+      windowsNumber=25;
       break;
     default:
   }
-
 }
-    for(var i = 0; i < board.length; i++){
-        //create a div HTML element called cell
-        var cell = document.createElement("div");
-        //set its CSS class to cell
-        cell.setAttribute("class", "cell");
-        cell.setAttribute("id", i);
-        //add the div HTML element to the stage
-        container.appendChild(cell);
-        //handle click
-        cell.addEventListener("click", clickHandler);
-    }
-    var entrence = document.createElement("img");
-    entrence.setAttribute("class", "entrence");
-    entrence.src = "./img/door.png";
-    container.appendChild(entrence);
-
-// create qaa div
-    var qAndA = document.createElement("div");
-    qAndA.setAttribute("class", "qAndA");
-    qAndA.innerHTML = "Live saved: "+ saved;
-
-    frame.appendChild(qAndA);
-function clickHandler(){
-      current = board[this.id]
-      var str = board[this.id][1];
-      // qAndA.innerHTML = "<div>" + str + "</div>" +"<br>"+ "a) " + wrapInAdiv(board[this.id][3][0])
-      // + "<br>" + "b) " + wrapInAdiv(board[this.id][3][1]) + "<br>" +  "c) " + wrapInAdiv(board[this.id][3][2]);
-      var answer = prompt(str);
-if (answer == board[this.id][2]) {
-  alert("true");
-  this.style.backgroundColor = "blue";
-  this.removeEventListener("click", clickHandler);
-      if(board[this.id][4]==true){
-        ++saved;
-        qAndA.innerHTML = "Live saved: "+ saved + "<br>" + "Death: "+ death + "<br>" + "Empty" + empty;
-        this.style.backgroundColor = "green";
-      }else{
-        ++empty;
-        qAndA.innerHTML = "Live saved: "+ saved + "<br>" + "Death: "+ death + "<br>" + "Empty" + empty;s
-      }
-  //   // if false => mark black, add score, deactivate, do not change player
-}else{
-  alert("false");
-  this.style.backgroundColor = "blue";
-  this.removeEventListener("click", clickHandler);
-  if(board[this.id][4]==true){
-    ++death;
-    qAndA.innerHTML = "Live saved: "+ saved + "<br>" + "Death: "+ death + "<br>" + "Empty" + empty;
-    this.style.backgroundColor = "black";
-  }else{
-    ++empty;
-    qAndA.innerHTML = "Live saved: "+ saved + "<br>" + "Death: "+ death + "<br>" + "Empty" + empty;
+//init();
+function init(players){        // initialization of the game
+  var intro = document.getElementById('intro');
+  intro.remove();                                 //removing intro buttons and text
+  var block = document.createElement("div");      // creating block container
+  block.setAttribute("class", "container");
+  block.setAttribute("id", "container");
+  frame.appendChild(block);
+  buildingConstructor(players);
+  for(var i=0; i<windowsNumber;i++){
+    var isPersonInside = false
+    var a = rand(0, 60), b = rand(0, 60), factor = rand(0,5);
+    if(factor>0){
+      isPersonInside = true;}else isPersonInside = false;
+    var answers = [a+b-1, b+a+1, a+b];
+    arr_qa.push(["Calculate: " + a + " + " + b, a+b, answers, isPersonInside]);
+    board.push([i,arr_qa[i][0],arr_qa[i][1],arr_qa[i][2],arr_qa[i][3]] ); //pushing index of question and the question and the answer
   }
-  //   // if false => mark black, deactivate, change player
+  for(var i = 0; i < board.length; i++){
+    //create a div HTML element called cell
+    var cell = document.createElement("div");
+    //set its CSS class to cell
+    cell.setAttribute("class", "cell");
+    cell.setAttribute("id", i);
+          //add the div HTML element to the stage
+    block.appendChild(cell);
+          //handle click
+    cell.addEventListener("click", clickHandler);
+    }
+  var entrence = document.createElement("img");
+  entrence.setAttribute("class", "entrence");
+  entrence.src = "./img/door.png";
+  block.appendChild(entrence);
+
+  var stat = document.createElement("div");        // create status div
+  stat.setAttribute("class", "stat");
+  stat.setAttribute("id", "stat");
+  //output(stat, qAndA, gameInfo, saved, death, empty);
+
+  var players_stat = document.createElement("div");        // create status div
+  players_stat.setAttribute("class", "players_stat");
+  players_stat.setAttribute("id", "players_stat");
+  output(stat, qAndA, gameInfo, saved, death, empty, players_stat);
+                                                   // highlite active player
+  document.getElementById('player1').style.backgroundColor = "yellow";
 }
-    console.log("worked");
+function clickHandler(){
+  current = board[this.id];
+  qAndA.innerHTML = "<br>" + current[1] + "<br>" +   //asking question - Calculate...
+    "<br>"+ "a) " + wrapInAdiv(current[3][0], this.id) +
+    "<br>" + "b) " + wrapInAdiv(current[3][1], this.id) +
+    "<br>" +  "c) " + wrapInAdiv(current[3][2], this.id);
+  stat.appendChild(qAndA);
+  if(firstPlayer){                                                    // highlite active player
+    document.getElementById('player1').style.backgroundColor = "yellow";
+    document.getElementById('player2').style.backgroundColor = "transparent";
+    firstPlayer = false;
+  }else{
+    document.getElementById('player2').style.backgroundColor = "yellow";
+    document.getElementById('player1').style.backgroundColor = "transparent";
+  firstPlayer = true;}
 }
-// function wrapInAdiv(value){
-//   return "<span class='answers' onclick='checkAnswer("+value+")'>" + value + "</span>" + "<br>";
-// }
-// function checkAnswer(value){
-// if(current[2]==value){
-//   alert("true");
-//   board.style.backgroundColor = "green";
-//
-//   // if true => mark green, deactivate, dont change the player
-// }else{
-//   alert("false");
-//   // if false => mark black, deactivate, change player
-// }
-// }
+
+function wrapInAdiv(value, id){
+  return "<span class='answers' onclick='checkAnswer(" + value +","+ id + ")'>" + value  + "</span>" + "<br>";
+}
+function checkAnswer(value, id){
+  curr_window = document.getElementById(id);
+  if(current[2]==value){    // check if answer true
+    //alert("true");
+    curr_window.removeEventListener("click", clickHandler);
+      if(current[4]==true){     //check if person inside
+          ++saved;
+          firstPlayer ?  ++saved2 : ++saved1;
+          output(stat, qAndA, gameInfo, saved, death, empty, players_stat);
+          curr_window.style.backgroundColor = "green";
+          curr_window.style.backgroundImage = "none";
+      }else{
+          ++empty;
+          firstPlayer ?  ++empty2 : ++empty1;
+          output(stat, qAndA, gameInfo, saved, death, empty, players_stat);
+          curr_window.style.backgroundColor = "black";
+          curr_window.style.backgroundImage = "none";
+      }
+  // if true => mark green, deactivate, dont change the player
+}else{
+  //alert("false");
+    curr_window.removeEventListener("click", clickHandler);
+    if(current[4]==true){
+      ++death;
+      firstPlayer ?  ++death2 : ++death1;
+      output(stat, qAndA, gameInfo, saved, death, empty, players_stat);
+      curr_window.style.backgroundColor = "red";
+      curr_window.style.backgroundImage = "none";
+    }else{
+      ++empty;
+      firstPlayer ?  ++empty2 : ++empty1;
+      output(stat, qAndA, gameInfo, saved, death, empty, players_stat);
+      curr_window.style.backgroundColor = "black";
+      curr_window.style.backgroundImage = "none";
+    }
+//  if false => mark black, deactivate, change player
+}
+if(death+saved+empty==windowsNumber){
+  let total = ((saved/(saved+death))*100);
+  if(!onePlayer){
+    if(death == 0){
+      qAndA.innerHTML = "GRAULACJE! Wspólnie ocaliliście wszystkich mieszkańców znajdujących się w budynku :) <br>KONIEC(odśwież stronę by zagrać ponownie)";
+      stat.appendChild(qAndA);
+    }else if(saved == 0){
+      qAndA.innerHTML = ("Wszyscy potrzebujący pomocy zginęli... <br>KONIEC￼(odśwież stronę by zagrać ponownie)￼");
+      stat.appendChild(qAndA);
+    }else {
+      qAndA.innerHTML = ("Wspólnie ocaliliście " + total.toFixed(0) + "% mieszkańców znajdujących się w budynku <br>KONIEC￼(odśwież stronę by zagrać ponownie)");
+      stat.appendChild(qAndA);
+  }
+  document.getElementById('player1').style.backgroundColor = "transparent";
+  document.getElementById('player2').style.backgroundColor = "transparent";
+}else{
+  if(death == 0){
+    qAndA.innerHTML = "GRAULACJE! Ocaliłeś wszystkich mieszkańców znajdujących się w budynku :) <br>KONIEC(odśwież stronę by zagrać ponownie)";
+    stat.appendChild(qAndA);
+  }else if(saved == 0){
+    qAndA.innerHTML = ("Wszyscy potrzebujący pomocy zginęli... <br>KONIEC￼(odśwież stronę by zagrać ponownie)￼");
+    stat.appendChild(qAndA);
+  }else {
+    qAndA.innerHTML = ("Ocaliłeś " + total.toFixed(0) + "% mieszkańców znajdujących się w budynku <br>KONIEC￼(odśwież stronę by zagrać ponownie)");
+    stat.appendChild(qAndA);
+  }
+}
+}
+}
