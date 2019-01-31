bcb720a72dd34a3402cd340aee4a6f1d3c3d64af
kl4532 <kornellukasiak@gmail.com>
2019-01-31 10:59:08 +0100

    CRUD

--- node/blog-admin/app.js
+++ node/blog-admin/app.js
@@ -2,14 +2,38 @@ const express = require('express');
 const path = require('path');
 var mysql = require('mysql');
 var bodyParser =require('body-parser');
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
 // Init App
 const app = express();
 //middleware
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 //set static folder
-app.use(express.static(path.join(__dirname, 'node_modules')));
-app.use('/assets', express.static('public'));
+app.use(express.static(path.join(__dirname, 'node_modules'))); // bootstrap and jq
+app.use(express.static(path.join(__dirname, 'public'))); // front end static
+//express session middleware
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false } // co to kurwa jest??!?
+}));
+// app.use(session({
+//   secret: 'keyboard cat',
+//   cookie: { maxAge: 60000 }
+// }));
+//express messages middleware
+app.use(flash());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+//express validator middleware
+app.use(expressValidator());﻿
+
+
 const db = mysql.createConnection({
    host     : 'localhost',
    user     : 'root',
@@ -27,6 +51,14 @@ db.connect((err) =>{
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//test flash
+app.get("/testflash", (req, res)=>{
+  req.flash('msg1', 'Flash mesage');
+  res.send('hello corny!');
+});
+app.get("/sikandar", (req, res)=>{
+  res.send(req.flash('msg1'));
+});
 // Home Route
 app.get('/', (req, res) => {
   let sql = 'SELECT * FROM articles';
@@ -54,6 +86,18 @@ app.get('/articles/add', (req, res)=>{
     title: 'Add Article'
   });
 });
+app.get('/article/edit/:id', (req, res)=>{
+  let sql = `SELECT * FROM articles WHERE id=${req.params.id}`;
+  let query = db.query(sql, (err, data) =>{
+    if(err) throw err;
+    console.log(data);
+    res.render('edit_article', {
+    title: 'Edit article',
+    article: data[0]
+    });
+  });
+});
+//update submit
 app.post('/articles/add', urlencodedParser, (req, res)=>{
   let data = req.body; // getting data from form
   let article = {title: data.title, author: data.author, body: data.body};
@@ -61,7 +105,25 @@ app.post('/articles/add', urlencodedParser, (req, res)=>{
   let query = db.query(sql, article, (err, result) =>{
     if(err) throw err;
     console.log(`article added succesfully`);
+    req.flash('success', 'Article added');
     res.redirect('/');
+  });
+});
+app.post('/articles/edit/:id', urlencodedParser, (req, res)=>{
+  let data = req.body; // getting data from form
+  let article = {title: data.title, author: data.author, body: data.body};
+  let sql = `UPDATE articles SET ? WHERE id=${req.params.id}`;
+  let query = db.query(sql, article, (err, result) =>{
+    if(err) throw err;
+    console.log(`article edited succesfully`);
+    res.redirect('/');
+  });
+});
+app.delete('/article/:id', (req,res)=>{
+  let sql = `DELETE FROM articles WHERE id=${req.params.id}`;
+  let query = db.query(sql, (err, data) =>{
+    if(err) throw err;
+    res.send('Succes');
   });
 });
 app.listen(3000, ()=>{
