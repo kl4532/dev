b04b15d55051132d9a8dccc077c4ba4127a9412c
kl4532 <kornellukasiak@gmail.com>
2019-02-02 20:02:47 +0100

    auth-page-boiler

---
+++ node/Passport-auth-BradTrav/config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  mongoURI: 'mongodb://corny:corny@test-shard-00-00-nphga.mongodb.net:27017,test-shard-00-01-nphga.mongodb.net:27017,test-shard-00-02-nphga.mongodb.net:27017/test?ssl=true&replicaSet=test-shard-0&authSource=admin&retryWrites=true'
+}
+
+
+// mlab
+// mongoURI: 'mongodb://corny:<zmienhaslo>@ds119734.mlab.com:19734/todo'
